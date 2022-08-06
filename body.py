264ab6469a89aa68b920b8411256f2952f64376f
Matyas <100067626+metjum@users.noreply.github.com>
2022-08-06 22:37:52 +0530

    Update body.py

--- body.py
+++ body.py
@@ -19,7 +19,7 @@ def make_a_tweet(api):
     # Below are the hashtags that we target now. you can add or remove hashtags to this list in the format given below
     accounts=["@Streamerbans"] 
     user=random.choice(accounts)
-    tweets = api.user_timeline(screen_name =random.choice(accounts) , count =5)
+    tweets = api.user_timeline(screen_name =random.choice(accounts) , count =2)
     
     for tweet in tweets:
         time.sleep(120)
@@ -54,7 +54,7 @@ def make_a_tweet(api):
 while True:
     print("inside the main function \n")
     make_a_tweet(api)
-    time.sleep(30*60)
+    time.sleep(5*60)
    
     print("......taking a break.....\n\n")
 print('\n\n\n                   out of loop                          ')
