70942303b3efac5da3b5456481f0786f5fff010f
Matyas <100067626+metjum@users.noreply.github.com>
2022-08-07 12:44:10 +0200

    Update body.py

--- body.py
+++ body.py
@@ -38,7 +38,7 @@ def make_a_tweet(api):
         url = "https://api.ivr.fi/twitch/resolve/" + names
         resp = requests.get(url)
         resp = resp.json()
-        message = names + " Partner: " + str(resp['partner'])
+        message = names + " | ⚒ Partner: " + str(resp['partner'] + "⚒")
         print(message)
         try:
             tweet.favorite()
