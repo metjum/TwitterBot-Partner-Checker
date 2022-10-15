322a7f5f7353a3fb4b9990dd8010c7f01d25a8ac
metju <100067626+metjum@users.noreply.github.com>
2022-10-15 21:27:42 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
-key = ['IVeKN4Dz9vjuzpFMmg8en2vaE',#paste api key here inside the bracket
-      'cuTBTHdUhE0VOOgBEh8O5BnnNRxVCHMQOIWYyXwG9dJON0872M',#api secret
-      '1554575269377318912-3zkVXwCaJUoakUCNXqT4r2UAiicskQ',#access key
-      'wH2KAzR904AEuH8QCdrZt61o4DFLeSMA54jGneCIzwoWV'#access key secret
+key = ['',#paste api key here inside the bracket
+      '',#api secret
+      '',#access key
+      ''#access key secret
 ]
